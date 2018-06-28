55fb262851d6eaa9acde32f912cb16a770dbdf2f
egoeast <das.east@mail.com>
2018-06-28 09:29:51 +0300

    test

---
+++ main.js
@@ -0,0 +1,11 @@
+
+/*
+//document.getElementById('cont').style.color = 'green'
+var cont = document.getElementById('cont')
+cont.onclick = function() {
+    alert( 'Спасибо' );
+};
+function countRabbits() {
+    document.getElementById('next').style.color = 'green';
+    alert( 'Спасибо' );
+}*/
