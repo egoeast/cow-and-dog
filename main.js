98885ddbc90702c3fd884ca2c94009ae7ded59a8
egoeast <das.east@mail.com>
2018-06-29 09:03:13 +0300

    test

--- main.js
+++ main.js
@@ -50,6 +50,7 @@ class BaseObject {
         this.width = params.width;
         this.height = params.height;
         this.location = new Point(params.x, params.y)
+        this.center = new Point(params.x + params.width/2, params.y + params.height /2);
     }
 
     get getX() {
@@ -79,11 +80,11 @@ class BaseObject {
       }
   */
     calculateDistanceToPoint(x, y) {
-        return Math.sqrt(Math.pow(this.getX - x, 2) + Math.pow(this.getY - y, 2));
+        return Math.sqrt(Math.pow(this.center.x - x, 2) + Math.pow(this.center.y - y, 2));
     }
 
     calculateVectorToPoint(x, y) {
-        return new Vector(this.getX - x, this.getY - y);
+        return new Vector(this.center.x - x, this.center.y - y);
     }
 }
 
@@ -91,6 +92,7 @@ class MovableObject extends BaseObject {
 
     move(vector) {
         this.location.setCoord = [this.getX + vector.getX, this.getY + vector.getY];
+        this.center.setCoord = [this.getX+ this.width / 2, this.getY+ this.height / 2];
         if (this.getX < 0) this.location.x = 0;
         if (this.getY < 0) this.location.y = 0;
         this.element.style.top = (this.getY ) + 'px';
@@ -174,93 +176,145 @@ class Cow extends MovableObject {
 
 
 window.onload = () => {
-
-    var cow = new Cow('cow-cicle', 80);
-    console.log(cow);
-    cow.drawShpere();
-
-    //cow.moveTo(50,50);
-
-
-    var content = document.getElementById('content');
-    var contX = document.getElementById('cont-x');
-    var contY = document.getElementById('cont-y');
-//const cow = document.getElementById('cow');
-    //var myType = document.getElementsByClassName('event-type');
+    /*var moving = false;
     var shprere = document.getElementById('cow-cicle');
-    shprere.onmouseover = handler;
-    shprere.onmousemove = handlerMove;
-    //cow.element.onmouseover = cow.element.onmouseout = cow.element.onmousemove = cowHandler;
-    shprere.onmouseout = shprereHandler;
+    shprere.onmouseover = handlerOver;
+    shprere.onmouseout = handlerOut;
 
-    var vector =new Vector();
-    /* function cowHandler(event) {
-
-         contX.innerText = event.clientX;
-         var type = event.type;
-         while (type < 11) type += ' ';
-
-         return false;
-     }*/
-
-    function handlerMove(event) {
-        vector = cow.calculateVectorToPoint(event.clientX, event.clientY);
-    }
-
-
-    function shprereHandler(event) {
-        cow.setState(COW_EATING);
-    }
-
-    function handler(event) {
-        vector = cow.calculateVectorToPoint(event.clientX, event.clientY);
-        //if (cow.calculateDistanceToPoint(event.clientX, event.clientY) <= cow.getSphere) {
-            cow.setState(COW_RUNNING);
-
-
-                let v = vector;
-                v.prepareDirection(50);
-                if (cow.state != COW_RUNNING)
-                    cow.setState(COW_RUNNING);
-                $( "#cow-cicle" ).animate({
-                    left: "+=" + v.getX,
-                    top: "+=" + v.getY
-                }, 300, function() {
-                    // Animation complete.
-                   // cow.setState(COW_EATING);
-                });
-         /*       let flag = false;
-            var fps = 50; // 50 кадров в секунду
-            var timer = setInterval(function () {
-                if (flag) {
+    function handlerOver(event) {
+        moving = true;
+        let x= 200
+        var fps = 200; // 50 кадров в секунду
+        var timer = setInterval(function () {
+                if (!moving) {
                     clearInterval(timer);
                 } else {
                     //let randX = Math.floor(Math.random() * 20) - 10
                     //let randY = Math.floor(Math.random() * 20) - 10
                     //cow.move((vector.getX + randX) / 5, (vector.getY + randY) / 5);
                     //let v = new Vector(vector.getX - cow.width, vector.getY - cow.width,)
-                    let v = vector;
-                    v.prepareDirection();
+                    let v = new Vector([1,1]);
+                    v.prepareDirection(1);
                     console.log(v);
-                    cow.move(v);
+                    shprere.style.left = x + 'px';
+                    x+=2
+                    //cow.move(v);
                     //cow.drawShpere();
                     //flag = true;
-                     if (cow.calculateDistanceToPoint(event.clientX, event.clientY) > cow.getSphere) {
-                          flag = true;
-
-                     }
                 }
             }, 1000 / fps)
-*/
-        //}
 
-        contX.innerText = vector.getX;
-        contY.innerText = vector.getY;
-        let vLength = document.getElementById('vector-length');
-        vLength.innerText = Math.sqrt(vector.getY*vector.getY+ vector.getX*vector.getX);
-        let vectorLog = document.getElementById('lineAB');
-        vectorLog.setAttribute('d', "M 200 200 l " + vector.getX + " " + vector.getY);
 
-        return false;
     }
+
+    function handlerOut(event) {
+        moving =false;
+    }*/
+
+
+     console.log(cow);
+     //cow.drawShpere();
+
+
+
+
+     var content = document.getElementById('content');
+     var contX = document.getElementById('cont-x');
+     var contY = document.getElementById('cont-y');
+ //const cow = document.getElementById('cow');
+     //var myType = document.getElementsByClassName('event-type');
+     var shprere = document.getElementById('cow-cicle');
+    var cow = new Cow('cow-cicle', 80);
+     shprere.onmouseover = handlerOver;
+     //shprere.onmouseout = handlerMove;
+     //cow.element.onmouseover = cow.element.onmouseout = cow.element.onmousemove = cowHandler;
+     shprere.onmouseout = handlerOut;
+    shprere.onmousemove = handlerMove;
+     var moving = false;
+
+     var vector =new Vector();
+     /* function cowHandler(event) {
+
+          contX.innerText = event.clientX;
+          var type = event.type;
+          while (type < 11) type += ' ';
+
+          return false;
+      }*/
+/*
+     function handlerMove(event) {
+         vector = cow.calculateVectorToPoint(event.clientX, event.clientY);
+     }*/
+
+
+     function handlerOut(event) {
+         moving = false;
+         cow.setState(COW_EATING);
+     }
+
+     function handlerMove(event) {
+         vector = cow.calculateVectorToPoint(event.clientX, event.clientY);
+         contX.innerText = vector.getX;
+         contY.innerText = vector.getY;
+         let vLength = document.getElementById('vector-length');
+         vLength.innerText = Math.sqrt(vector.getY*vector.getY+ vector.getX*vector.getX);
+         let vectorLog = document.getElementById('lineAB');
+         vectorLog.setAttribute('d', "M 200 200 l " + vector.getX + " " + vector.getY);
+
+     }
+
+     function handlerOver(event) {
+         vector = cow.calculateVectorToPoint(event.clientX, event.clientY);
+         //if (cow.calculateDistanceToPoint(event.clientX, event.clientY) <= cow.getSphere) {
+             cow.setState(COW_RUNNING);
+
+
+          /*       let v = vector;
+                 v.prepareDirection(50);
+          */
+          /*if (cow.state != COW_RUNNING)
+                     cow.setState(COW_RUNNING);*/
+        /*         $( "#cow-cicle" ).animate({
+                     left: "+=" + v.getX,
+                     top: "+=" + v.getY
+                 }, 300, function() {
+        */             // Animation complete.
+                    // cow.setState(COW_EATING);
+            //     });
+            moving = true;
+             var fps = 20; // 50 кадров в секунду
+             var timer = setInterval(function () {
+                 if (!moving) {
+                     clearInterval(timer);
+                 } else {
+                     //let randX = Math.floor(Math.random() * 20) - 10
+                     //let randY = Math.floor(Math.random() * 20) - 10
+                     //cow.move((vector.getX + randX) / 5, (vector.getY + randY) / 5);
+                     //let v = new Vector(vector.getX - cow.width, vector.getY - cow.width,)
+
+                     let v = vector;
+                     v.prepareDirection(5);
+                    cow.move(v);
+                     console.log(cow.center);
+
+                     //cow.drawShpere();
+                     //flag = true;
+                      /*if (cow.calculateDistanceToPoint(event.clientX, event.clientY) > cow.getSphere) {
+                           flag = true;
+
+                      }*/
+                 }
+             }, 1000 / fps)
+
+         //}
+
+         contX.innerText = vector.getX;
+         contY.innerText = vector.getY;
+         let vLength = document.getElementById('vector-length');
+         vLength.innerText = Math.sqrt(vector.getY*vector.getY+ vector.getX*vector.getX);
+         let vectorLog = document.getElementById('lineAB');
+         vectorLog.setAttribute('d', "M 200 200 l " + vector.getX + " " + vector.getY);
+
+         return false;
+     }
 }
